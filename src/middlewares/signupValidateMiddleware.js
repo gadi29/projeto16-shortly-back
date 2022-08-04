85231ebde4fbc9951f5f3e3ef94e7e885c4f875b
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-04 15:42:51 -0300

    adjusts in validate return error messages

--- src/middlewares/signupValidateMiddleware.js
+++ src/middlewares/signupValidateMiddleware.js
@@ -3,9 +3,9 @@ import newUserSchema from '../schemas/signupSchema.js';
 async function signupValidateMiddleware (req, res, next) {
   const newUser = req.body;
 
-  const validateUser = newUserSchema.validate(newUser);
+  const validateUser = newUserSchema.validate(newUser, { abortEarly: false });
   if (validateUser.error) {
-    return res.status(422).send(validateUser.error);
+    return res.status(422).send(validateUser.error.details);
   }
 
   res.locals.user = newUser;
