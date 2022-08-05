996a5772fb0b4476d620046cb0c901c4a439a03a
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-05 14:24:11 -0300

    import urlValidateMiddleware in urlsRoutes

--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import urlValidateMiddleware from "../middlewares/urlValidateMiddleware.js";
+
 const urlsRoutes = Router();
 
 urlsRoutes.post('/urls/shorten', urlValidateMiddleware, generateShortUrl);
