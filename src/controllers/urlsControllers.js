4aeaf2dad6b178f116d592704329597555b9c541
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-05 14:36:15 -0300

    create generateShortUrl function in urlsControllers

---
+++ src/controllers/urlsControllers.js
@@ -0,0 +1,20 @@
+import connection from '../dbStrategy/pgsql.js';
+import { nanoid } from 'nanoid';
+
+export async function generateShortUrl (req, res) {
+  const user = res.locals.user;
+  const url = res.locals.url;
+  const shortUrl = nanoid(8);
+
+  try {
+    await connection.query(
+      'INSERT INTO urls ("userId", "url", shortUrl") VALUES ($1, $2, $3)',
+      [user.id, url, shortUrl]
+    )
+
+    res.status(201).send({ shortUrl: shortUrl });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/urlsRoutes.js
+++ src/routes/urlsRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import urlValidateMiddleware from "../middlewares/urlValidateMiddleware.js";
+import { generateShortUrl } from "../controllers/urlsControllers.js";
 
 const urlsRoutes = Router();
 
