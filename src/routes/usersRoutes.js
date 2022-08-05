f8ed9c4551870590dd745e9ae0e0ecfefa87f249
Gadiel Zilto Azevedo <gad.azevedo@gmail.com>
2022-08-05 17:47:33 -0300

    create ranking route

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -6,5 +6,6 @@ import { getUserHistory } from "../controllers/usersControllers.js";
 const usersRoutes = Router();
 
 usersRoutes.get('/users/me', authenticateUser, getUserHistory);
+usersRoutes.get('/ranking', getRanking);
 
 export default usersRoutes;
